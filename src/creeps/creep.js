dd282f471d3b33bf31bc339c90a1a441bd2a5f82
Ari Velakoski <ondalf@gmail.com>
2016-08-28 15:26:02 +0300

    Fix harvester constructing
    Doesn't walk around to get next to the target

--- src/creeps/creep.js
+++ src/creeps/creep.js
@@ -266,7 +266,7 @@ Creep.prototype.runHarvester = function() {
         target = _.head(targetSource.pos.findInRange(
             FIND_MY_CONSTRUCTION_SITES, 2));
         if(target != null) {
-            if(this.pos.isNearTo(target)) {
+            if(this.pos.inRangeTo(target, 2)) {
                 this.build(target);
             } else {
                 this.moveToS(target);
